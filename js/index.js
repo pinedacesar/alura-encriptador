66f1924b102af4a5911bc97276943166213138cf
pinedacesar <cesar.pineda.assert@gmail.com>
2024-03-19 19:27:16 -0300

    Primer commit

---
+++ js/index.js
@@ -0,0 +1,77 @@
+const texto_entrada = document.querySelector('#texto-entrada');
+const texto_respuesta = document.querySelector('#texto-respuesta');
+
+const inicial = document.querySelector('.inicial');
+const final = document.querySelector('.final');
+
+document.addEventListener('keyup', () => {
+  let asento = 'ÁÃÀÄÂÉËÈÊÍÏÌÎÓÖÒÔÚÜÙÛÑÇáãàäâéëèêíïìîóöòôúüùûñç´',
+    sinAsento = 'AAAAAEEEEIIIIOOOOUUUUNCaaaaaeeeeiiiioooouuuunc',
+    remplazo = new RegExp('[' + asento + ']', 'ug');
+
+  texto_entrada.value = texto_entrada.value.replace(remplazo, (match) =>
+    sinAsento.charAt(de.indexOf(match))
+  );
+
+  texto_entrada.value = texto_entrada.value.toLowerCase();
+});
+
+const matriz_code = [
+  ['e', 'enter'],
+  ['i', 'imes'],
+  ['a', 'ai'],
+  ['o', 'ober'],
+  ['u', 'ufat'],
+];
+
+function btnEncriptar() {
+  const texto = encriptar(texto_entrada.value);
+  chequearInput(texto);
+}
+
+function encriptar(textoEncriptado) {
+  for (let i = 0; i < matriz_code.length; i++) {
+    if (textoEncriptado.includes(matriz_code[i][0])) {
+      textoEncriptado = textoEncriptado.replaceAll(
+        matriz_code[i][0],
+        matriz_code[i][1]
+      );
+    }
+  }
+  return textoEncriptado;
+}
+
+function btnDesencriptar() {
+  const texto = desencriptar(texto_entrada.value);
+  chequearInput(texto);
+}
+
+function desencriptar(textoDesencriptado) {
+  for (let i = 0; i < matriz_code.length; i++) {
+    if (textoDesencriptado.includes(matriz_code[i][0])) {
+      textoDesencriptado = textoDesencriptado.replaceAll(
+        matriz_code[i][1],
+        matriz_code[i][0]
+      );
+    }
+  }
+  return textoDesencriptado;
+}
+
+function chequearInput(texto) {
+  if (texto.trim() !== '') {
+    texto_respuesta.value = texto;
+    texto_entrada.value = '';
+    final.classList.remove('none');
+    inicial.classList.add('none');
+  } else {
+    alert('No has ingresado ningun texto');
+  }
+}
+
+function copiar() {
+  texto_respuesta.select();
+  navigator.clipboard.writeText(texto_respuesta.value);
+  inicial.classList.remove('none');
+  final.classList.add('none');
+}
